a94f0cda014b2584ada1aeaa05cd03d43f4779b1
Sarah Pickering <sarahpsellsaz@gmail.com>
2025-03-30 19:18:02 -0700

    change link color, add description

--- src/Components/MyProjects.js
+++ src/Components/MyProjects.js
@@ -5,15 +5,15 @@ import { SectionContainer } from './Styles/globalStyle';
 const projects = [
     { name: "Simple Calculator", 
         url: "https://github.com/sarahpickering/24w5-calculator", 
-        description: `Building and styling this calculator was an exciting 
-        challenge during my studies. It reinforced my understanding of 
+        description: `Building and styling this calculator while at 
+        Bloomtech was exciting and challenging. It reinforced my understanding of 
         JavaScript logic and CSS styling while giving me hands-on 
         experience with interactive user interfaces.` },
    // { name: "Project 2", url: "https://github.com/yourusername/project2" }
 ];
 
 const StyledLink = styled.a`
-    color: #2B2B28;
+    color:#0000EE;
     text-decoration: none;
     font-weight: bold;
 
@@ -26,11 +26,14 @@ const MyProjects = () => (
     <SectionContainer>
         <h1>My Favorite Projects:</h1>
         {projects.map((project, index) => (
-            <p key={index}>
+            <div key={index}>
+                <p>
                 <StyledLink href={project.url} target="_blank" rel="noopener noreferrer">
                     {project.name}
                 </StyledLink>
-            </p>
+                </p>
+                {project.description && <p>{project.description}</p>}
+                </div>
         ))}
     </SectionContainer>
 );
